e59d48967f04242d28d4867e935e9b67f6bb3da8
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-05-28 02:55:07 -0300

    Add files via upload

--- Bascara.py
+++ Bascara.py
@@ -5,13 +5,15 @@ b = float(input("b: "))
 c = float(input("c: "))
 if b**2 - 4*a*c>=0:
 	deltaSq = math.sqrt(b**2 - 4*a*c)
-	x1 = (-b + deltaSq)/2
-	x2 = (-b - deltaSq)/2
+	x1 = (-b + deltaSq)/2/a
+	x2 = (-b - deltaSq)/2/a
 	if deltaSq == 0:
 		print("solucao unica ->", x1)
 	else:
 		print("solucao 1 ->", x1)
 		print("solucao 2 ->", x2)
 else:
-	print("A solução não pertence ao conjunto dos numeros reais")
+	deltaSqImg = math.sqrt(4*a*c - b**2)	
+	print("solucao 1 ->", -b/2/a, " + i *", deltaSqImg/2/a)
+	print("solucao 2 ->", -b/2/a, " - i *", deltaSqImg/2/a)
 print("----\n")
